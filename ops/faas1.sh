1e193373b8dbb2bedc517cb0fbc3d6f1183bf4e7
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-01-28 18:49:33 +0100

    add portainer

--- ops/faas1.sh
+++ ops/faas1.sh
@@ -30,6 +30,9 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
+#deploy portainer
+docker run -d -p 9000:9000 --restart always -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer
+
 #faas
 [ -d /usr/local/faas ] || {
   pushd /usr/local
